86b4786d17298ddcf7901492be9ac08f5229de3f
Thomas Krieger <tom@tom-krieger.de>
2021-12-03 21:19:22 +0100

    debug output

--- lib/puppet/functions/getdevices.rb
+++ lib/puppet/functions/getdevices.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 Puppet::Functions.create_function(:getdevices) do
   dispatch :devices do
     required_param 'Hash', :partitioning
@@ -6,7 +8,9 @@ Puppet::Functions.create_function(:getdevices) do
 
   def devices(partitioning)
     device_list = []
+    pp partitioning
     partitioning.each do |part, part_data| 
+      pp part_data
       if part_data.key?('device')
         device_list.push($part_data['device'])
       end
