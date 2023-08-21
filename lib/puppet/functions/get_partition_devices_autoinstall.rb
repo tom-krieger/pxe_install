66821811d9104e6593b5f00237eea66710691f4f
Thomas Krieger <tom@tom-krieger.de>
2023-08-21 14:20:20 +0200

    linting

--- lib/puppet/functions/get_partition_devices_autoinstall.rb
+++ lib/puppet/functions/get_partition_devices_autoinstall.rb
@@ -4,23 +4,22 @@
 # Examin partitioning information of Debian/Ubuntu hosts and get all
 # used disk devices
 Puppet::Functions.create_function(:get_partition_devices_autoinstall) do
-    dispatch :get_partition_devices_autoinstall do
-      required_param 'Array', :partitioning
-      return_type 'Array'
-    end
-  
-    def get_partition_devices_autoinstall(partitioning)
-      return 'fail' if partitioning.empty?
-      return 'nil' if partitioning.nil?
-  
-      device_list = []
-      partitioning.each do |part_data|
-        if part_data.key?('device')
-          device_list.push(part_data['device'])
-        end
-      end
-      return 'failed' if device_list.empty?
-      device_list.uniq unless device_list.empty?
-    end
+  dispatch :get_partition_devices_autoinstall do
+    required_param 'Array', :partitioning
+    return_type 'Array'
   end
-  
+
+  def get_partition_devices_autoinstall(partitioning)
+    return 'fail' if partitioning.empty?
+    return 'nil' if partitioning.nil?
+
+    device_list = []
+    partitioning.each do |part_data|
+      if part_data.key?('path') && part_data['path'].match(%r{^\/dev})
+        device_list.push(part_data['path'])
+      end
+    end
+    return 'failed' if device_list.empty?
+    device_list.uniq unless device_list.empty?
+  end
+end
